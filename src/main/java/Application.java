b13875f44adc80b4b2d39adb3afb04b7f8d71713
zebzeev-sv <sergey.zebzeev@gmail.com>
2019-08-29 23:29:17 +0500

    вариант для дарта

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,4 @@
-import javax.mail.Folder;
-import javax.mail.Session;
-import javax.mail.Store;
+import javax.mail.*;
 import java.util.Properties;
 
 /**
@@ -9,34 +7,39 @@ import java.util.Properties;
  */
 public class Application {
 
-	final static String host = "imap.gmail.com";
-	final static String login = "sergey.zebzeev@gmail.com";
-	final static String password = "iltdpodznfmzwkye";
+	final static String host = "mail.dartit.ru";
+	final static String login = "";
+	final static String password = "&";
 
 
 	public static void main(String[] args) {
 		connectMail();
 	}
 
-	private static void connectMail () {
+	private static void connectMail() {
 
 		Properties properties = System.getProperties();
 		properties.setProperty("mail.imaps.port", "993");
-		properties.put("mail.imap.ssl.enable", "true");
 		properties.setProperty("mail.imaps.connectiontimeout", "5000");
 		properties.setProperty("mail.imaps.timeout", "5000");
 
-		try{
+		try {
 			Session session = Session.getInstance(properties);
-			session.setDebug(false);
+			session.setDebug(true);
 			Store store = session.getStore("imaps");
 			store.connect(host, login, password);
 			Folder inbox = store.getFolder("INBOX");
 			inbox.open(Folder.READ_ONLY);
-			System.out.println("Количество сообщений : " +inbox.getMessageCount());
-		}catch (Exception ex) {
+			System.out.println("Количество сообщений : " + inbox.getMessageCount());
+
+			for (Message message : inbox.getMessages()) {
+				System.out.println("Message: " + message.getFrom()[0].getType());
+				Address address = message.getFrom()[0];
+				System.out.println("From: " + message.getSubject());
+			}
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-
 	}
 }
